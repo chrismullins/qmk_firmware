2b68fe29c7aa7b4896c979e770a5b8bcf2734d1d
Christopher Mullins <christopherrmullins@gmail.com>
2019-08-12 19:33:46 -0700

    Convenience keys for the bottom row

--- keyboards/jd40/keymaps/chrismullins/keymap.c
+++ keyboards/jd40/keymaps/chrismullins/keymap.c
@@ -22,8 +22,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BL] = LAYOUT(
         TD(TD_TAB_ESC),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_RSFT,
-        MO(_AL), KC_LALT, KC_LALT, KC_LGUI, KC_SPC,  _______,  KC_SPC, KC_DOWN, KC_UP, MO(_AL)
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, RSFT_T(KC_SLSH),
+        MO(_AL), KC_LALT, KC_LALT, KC_LGUI, MO(_AL),  _______,  KC_SPC, KC_DOWN, KC_UP, MO(_AL)
     ),
 
     [_AL] = LAYOUT(
